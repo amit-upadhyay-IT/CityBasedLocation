4b1dd41e549292bf661ab61768f6d6f78427522a
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-03-16 18:47:51 +0530

    modularized the code and separated LocationFinder login

---
+++ app/src/main/java/com/amitupadhyay/citybasedlocation/LocationFinder.java
@@ -0,0 +1,163 @@
+package com.amitupadhyay.citybasedlocation;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+/**
+ * Created by aupadhyay on 3/16/17.
+ */
+
+public class LocationFinder implements LocationListener {
+
+    private Context context;
+    private OnLocationFoundListener onLocationFoundListener;
+
+    public LocationFinder(Context context) {
+        this.context = context;
+        onLocationFoundListener = (OnLocationFoundListener) context;
+    }
+
+    private static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
+
+    private LocationManager locationManager;
+    private ProgressDialog progressDialog;
+
+
+    void getCityByLocation() {
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity)context,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Explanation not needed, since user requests this himself
+
+            } else {
+                ActivityCompat.requestPermissions((Activity)context,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_ACCESS_FINE_LOCATION);
+            }
+
+        } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            progressDialog = new ProgressDialog(context);
+            progressDialog.setMessage(context.getString(R.string.getting_location));
+            progressDialog.setCancelable(false);
+            progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, context.getString(R.string.dialog_cancel), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    try {
+                        locationManager.removeUpdates(LocationFinder.this);
+                    } catch (SecurityException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            progressDialog.show();
+            if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            }
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+        } else {
+            showLocationSettingsDialog();
+        }
+    }
+
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        progressDialog.hide();
+        try {
+            locationManager.removeUpdates(this);
+        } catch (SecurityException e) {
+            Log.e("LocationManager", "Error while trying to stop listening for location updates. This is probably a permissions issue", e);
+        }
+        Log.i("LOCATION (" + location.getProvider().toUpperCase() + ")", location.getLatitude() + ", " + location.getLongitude());
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        getCityDetails(latitude, longitude);
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private void getCityDetails(double lat, double lon)
+    {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(lat, lon, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String cityName = addresses.get(0).getAddressLine(0);
+        String stateName = addresses.get(0).getAddressLine(1);
+        String countryName = addresses.get(0).getAddressLine(2);
+
+        progressDialog.dismiss();
+        onLocationFoundListener.setOnLocationFoundListener(cityName, stateName, countryName);
+
+    }
+
+    private void showLocationSettingsDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle(R.string.location_settings);
+        alertDialog.setMessage(R.string.location_settings_message);
+        alertDialog.setPositiveButton(R.string.location_settings_button, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                context.startActivity(intent);
+            }
+        });
+        alertDialog.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
+
+    public interface OnLocationFoundListener
+    {
+        void setOnLocationFoundListener(String cityName, String stateName, String countryName);
+    }
+}
--- app/src/main/java/com/amitupadhyay/citybasedlocation/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/citybasedlocation/MainActivity.java
@@ -1,167 +1,39 @@
 package com.amitupadhyay.citybasedlocation;
 
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
+public class MainActivity extends AppCompatActivity implements LocationFinder.OnLocationFoundListener {
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-
-    protected static final int MY_PERMISSIONS_ACCESS_FINE_LOCATION = 1;
-
-    LocationManager locationManager;
-    ProgressDialog progressDialog;
-
+    Button getCurrentLocation;
+    TextView locationTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDialog = new ProgressDialog(MainActivity.this);
-
-        Button getCurrentLocation = (Button) findViewById(R.id.getCurrentCity);
+        getCurrentLocation = (Button) findViewById(R.id.getCurrentCity);
+        locationTextview = (TextView) findViewById(R.id.current_city);
 
         getCurrentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getCityByLocation();
+
+                LocationFinder locationFinder = new LocationFinder(MainActivity.this);
+                locationFinder.getCityByLocation();
+
             }
         });
 
-    }
-
-    void getCityByLocation() {
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Explanation not needed, since user requests this himself
-
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_ACCESS_FINE_LOCATION);
-            }
-
-        } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.setMessage(getString(R.string.getting_location));
-            progressDialog.setCancelable(false);
-            progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.dialog_cancel), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    try {
-                        locationManager.removeUpdates(MainActivity.this);
-                    } catch (SecurityException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            progressDialog.show();
-            if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-            }
-            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-            }
-        } else {
-            showLocationSettingsDialog();
-        }
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        progressDialog.hide();
-        try {
-            locationManager.removeUpdates(this);
-        } catch (SecurityException e) {
-            Log.e("LocationManager", "Error while trying to stop listening for location updates. This is probably a permissions issue", e);
-        }
-        Log.i("LOCATION (" + location.getProvider().toUpperCase() + ")", location.getLatitude() + ", " + location.getLongitude());
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        getCityDetails(latitude, longitude);
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
 
     }
 
     @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    private void getCityDetails(double lat, double lon)
-    {
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        List<Address> addresses = null;
-        try {
-            addresses = geocoder.getFromLocation(lat, lon, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String cityName = addresses.get(0).getAddressLine(0);
-        String stateName = addresses.get(0).getAddressLine(1);
-        String countryName = addresses.get(0).getAddressLine(2);
-
-        setCityDetails(cityName, stateName, countryName);
-
-    }
-
-    private void setCityDetails(String cityName, String stateName, String countryName)
-    {
-        TextView tv = (TextView) findViewById(R.id.current_city);
-
-        tv.setText("City : "+cityName+", "+"\nState : "+stateName+", "+"\nCountry : "+countryName);
-    }
-
-
-    private void showLocationSettingsDialog() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle(R.string.location_settings);
-        alertDialog.setMessage(R.string.location_settings_message);
-        alertDialog.setPositiveButton(R.string.location_settings_button, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        });
-        alertDialog.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        alertDialog.show();
+    public void setOnLocationFoundListener(String cityName, String stateName, String countryName) {
+        locationTextview.setText("City : "+cityName+", "+"\nState : "+stateName+", "+"\nCountry : "+countryName);
     }
 }
